19d5dfa94326aa826327a1e35e347f600bdf4a96
RasenRhino <ridhambhagat@gmail.com>
2020-11-20 02:24:51 +0530

    bst done

--- Tree.java
+++ Tree.java
@@ -11,85 +11,176 @@ import javafx.scene.shape.Circle;
  * @author sam
  */
 //implements BST functionality
-class node{
+class Node{
     public int key;  
-    public node left;
-    public node right;
+    public Node left;
+    public Node right;
+    Node(int key){
+        this.key=key;
+        this.left=null;
+        this.right=null;
+    }
+    Node(){
+        this.left=null;
+        this.right=null;
+    }
 }
+
 public class Tree {
     
-    public node root; 
+    public static Node root; 
     public int height;
     public int noOfNodes;
     //functions to be implemented
 
     //default constructor
     Tree()
-    {
-        //initialize, root, height, no of nodes of tree
-        //code here
+    {   root=null;
+        
     }
     //constructor with arraylist of nodes
-    Tree (ArrayList<Integer> lst)
-    {
-        //code here
-    }
     //insert an element in BST
-    void insert(int element)
-    {
-        node n = root;
-        // code here
-        if(root == null)
-            root.key = element;  
-        else
-        {
-            if (element < n.key) 
-                 n = n.left;
-            else if (element > n.key) 
-                n = n.right;
+    Node insertTree(Node root, int key){kl
+        if(root==null){
+            Node leaf=new Node(key);
+            root=leaf;
+            return root;
         }
-        n.key = element;
+        else{
+            if (key < root.key) 
+            root.left = insertTree(root.left, key); 
+                else if (key > root.key) 
+            root.right = insertTree(root.right, key); 
+
+        /* return the (unchanged) node pointer */
+        return root; 
+        }
+    }
+    int height(){
+        return (treeHeight(root));
+    }
+    int treeHeight(Node root){
+        if(root == null){
+            return 0;
+        }
+        int left=treeHeight(root.left)
+;       int right=treeHeight(root.right);
+        if(left>right){
+            return (left+1);
+        }
+        else{
+            return (right+1);
+        }
+    }
+    void insert(int element)
+    {   
+        root=insertTree(root,element);
+        height=height();
+        // code here
+    }
+    Node searchTree(Node root,int key){
+        if (root==null || root.key==key) 
+        {   return root; 
+            }
+        if (root.key < key) 
+            {
+            return SearchTree(root.right, key); 
+            }
+        return SearchTree(root.left, key); 
     }
     //searching an element
     boolean search(int element)
     {
-        // code here
-        //return true or false
-        return true;
+        if(searchTree(root,element)!=null)
+            return true;
+        else
+            return false;
+    }
+    ArrayList<Integer> preorder(ArrayList<Integer> p,Node root)
+    {
+        if (root != null) { 
+            p.add(root.key); 
+            preorder(p,root.left); 
+            preorder(p,root.right); 
+        } 
+        return p;
     }
     //deleting an element
-    void delete(int element)
-    {
-        // code here
+ void delete(int elem){
+        root=deleteTree(root,elem);
+    }
+    Node insertDel(Node root,Node leaf){
+        if(root==null){
+            root=leaf;
+            return root;
+        }
+        else{
+            if (leaf.key < root.key) 
+            root.left = insertDel(root.left, leaf); 
+                else if (leaf.key > root.key) 
+            root.right = insertDel(root.right, leaf); 
+
+        return root; 
+        }
+    }
+    Node deleteTree(Node root,int elem){
+        if(elem< root.key){
+            root.left=deleteTree(root.left,elem);
+        }
+        else if(elem>root.key){
+            root.right=deleteTree(root.right,elem);
+        }
+        else if(elem==root.key &&(root.left==null && root.right==null)){
+ 
+            if(root.left==null && root.right==null){
+                root=null;
+            }
+        }
+        else if(elem==root.key &&(root.right!=null && root.left!=null)){
+            root.right=insertDel(root.right,root.left);
+            root=root.right;
+
+        }
+        else if(elem==root.key &&(root.right!=null && root.left==null)){
+            root=root.right;
+        }
+        else if(elem==root.key &&(root.right==null && root.left!=null)){
+            root=root.left;
+        }
+        return root;
     }
     //preorder traversal
-    ArrayList<Integer> preorder()
+    ArrayList<Integer> inorder(ArrayList<Integer> p,Node root)
     {
-         // code here
-        return null;
+        if (root != null) { 
+            inorder(p,root.left); 
+            p.add(root.key); 
+            inorder(p,root.right); 
+        } 
+        return p;
     }
     //postorder traversal
-    ArrayList<Integer> postorder()
+    ArrayList<Integer> postorder(ArrayList<Integer> p,Node root)
     {
-         // code here
-        return null;
+        if (root != null) { 
+            postorder(p,root.left); 
+            postorder(p,root.right); 
+            p.add(root.key); 
+            
+        } 
+        return p;
     }
     //inorder traversal
-    ArrayList<Integer> inorder()
-    {
-         // code here
-        return null;
-    }
+    
     //clear the tree 
     void clear()
     {
-         // code here
+         root=null;
     }
     //is empty
     boolean isEmpty()
-    {
-         // code here
-        return true;
+    {   if(root == null) return true;
+        return false;
     }
     
 }
