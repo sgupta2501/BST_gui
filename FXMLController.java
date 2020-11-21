d75ca139a9cb2ff17f831eb672843eed31e0c033
sgupta2501 <sgupta2501@gmail.com>
2020-11-22 00:11:05 +0530

    Add files via upload

--- FXMLController.java
+++ FXMLController.java
@@ -18,9 +18,12 @@ import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;  
+import javafx.geometry.Point2D;
 
 /**
  * FXML Controller class
@@ -29,6 +32,8 @@ import javafx.scene.shape.Circle;
  */
 
 import javafx.scene.canvas.*; 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
 /**
  *
@@ -36,15 +41,160 @@ import javafx.scene.canvas.*;
  */
 class GTree extends Canvas{
     public Tree tree;
-    
+    public String listTraversed;
+    public int maxHeightAllowed;
+    public Color defaultColor;
+
     GTree()
     {
         tree=new Tree();
+        listTraversed="";
+        maxHeightAllowed=6;
+        defaultColor=Color.LIGHTGREEN;
     }
-}
+        //render elements of the tree on GUI
+    void draw()
+    {
+	GraphicsContext gc = getGraphicsContext2D();
+	gc.clearRect(0, 0, this.getWidth(), this.getHeight());
+
+	// If the tree is not empty; draw the lines and circles
+	if (tree.root != null) 
+        {
+            int ht = tree.height;
+            double diameter, xMin=100, xMax, yMin=0, yMax;
+            int divBy = maxHeightAllowed + 3;
+            yMax= this.getHeight();
+            xMax= this.getWidth()-100;
+            diameter= yMax / divBy;
+            Point2D center= new Point2D(((xMin + xMax) / 2), (yMin + diameter/2));//skipping first division 
+            drawTree(gc, tree.root, xMin, xMax, center, diameter);
+	}    
+    }
+    
+    void drawTree(GraphicsContext gc, Node treeNode, double xMin, double xMax, Point2D center, double diameter) 
+    {
+        // Set the Color
+        gc.setStroke(Color.BROWN);
+        
+        if(treeNode!=null)
+        {
+            // Set fill color
+            gc.setFill(treeNode.color);
+            gc.fillOval(center.getX()-diameter/4,center.getY()-diameter/4, diameter/2, diameter/2);    
+            treeNode.color = defaultColor;
+            gc.strokeText(""+treeNode.key, center.getX()-6,center.getY()+5);
+            Point2D center2;
+            if (treeNode.left != null) {
+                    // Determine the start and end points of the line                 
+                    center2 = new Point2D((xMin+center.getX())/2,(center.getY()+diameter));
+                    gc.strokeLine(center.getX()-diameter/5,center.getY()+diameter/8,center2.getX(),center2.getY());
+
+                    // Recurse left circle nodes
+                    drawTree(gc, treeNode.left, xMin, center.getX(), center2, diameter);
+            }
+
+            // If right node is not null then draw a line to it
+            if (treeNode.right != null) {
+                    // Determine the start and end points of the line                 
+                    center2 = new Point2D((center.getX()+xMax)/2,(center.getY()+diameter));
+                    gc.strokeLine(center.getX()+diameter/5,center.getY()+diameter/8,center2.getX(),center2.getY());
+
+                    // Recurse left circle nodes
+                    drawTree(gc, treeNode.right, center.getX(), xMax, center2, diameter);
+                    
+            }
+        }
+    }
+
+    void storeList(ArrayList<Integer> l, char rev)
+    {
+        //convert array list to string
+        listTraversed = "";
+        if (l.isEmpty())
+            listTraversed=" Empty tree";
+        else {
+            if(rev == 'r')
+            {
+                for(int i = l.size()-1; i>=0; i--)
+                {
+                    listTraversed += l.get(i).toString() +"  ";
+                }
+            }
+            else
+            {
+                for (Integer a:l)
+                {
+                    listTraversed += a.toString() +"  ";
+                }   
+            }
+        }
+    }
+    
+    void insert(int element)
+    {
+        tree.insert(element);
+        draw();
+    }
+    //searching an element
+    String search(int element)
+    {
+        String str;
+        boolean b=tree.search(element);
+        draw();
+        str=element+"";
+        if (b==false)
+            str = str + "  Not found";
+        else str=str+"  Found";
+        return str;
+    }
+    //deleting an element
+    String delete(int element)
+    {
+        String str;
+        boolean b=tree.delete(element);
+        // node not found case to be handled
+        draw(); 
+        str=element+"";
+        if (b==false)
+            str = str + "  Not found";
+        else str = str + " Deleted";
+        return str;
+    }
+    //preorder traversal
+    void preorder()
+    {
+        ArrayList<Integer> l=new ArrayList<>();
+        l= tree.preorder(l,tree.root);
+        storeList(l,' ');
+        draw();
+    }
+    //postorder traversal
+    void postorder()
+    {
+        ArrayList<Integer> l=new ArrayList<>();
+        l= tree.postorder(l,tree.root);
+        storeList(l,' ');
+        draw();
+    }
+    //inorder traversal
+    void inorder(char rev)
+    {
+        ArrayList<Integer> l=new ArrayList<>();
+        l= tree.inorder(l,tree.root);
+        storeList(l,rev);
+        draw();
+    }
+    
+    //clear the tree 
+    void clear()
+    {
+        tree.clear();//should empty the tree
+        draw();
+    }
+  }
 public class FXMLController implements Initializable{
     
-    @FXML private Text actiontarget;
     // Panels and other GUI components
     @FXML private BorderPane rootPane;
     @FXML private TextArea valueList;
@@ -73,40 +223,81 @@ public class FXMLController implements Initializable{
             // Bind canvas size to stack pane size.
             gtree.widthProperty().bind(rootPane.widthProperty());
             gtree.heightProperty().bind(rootPane.heightProperty().subtract(50));
+
+            gtree.clear();
+            valueList.setText("");
+            counters.setText("Height: "+gtree.tree.height + " No. of Nodes: "+gtree.tree.noOfNodes);
+            
 	}
     
     @FXML protected void insertButton(ActionEvent event) {
        String strV=value.getText();
-       //Circle newC = new Circle(parseInt(strV));
-       gtree.tree.insert(parseInt(strV));
-       valueList.setText(valueList.getText()+" "+strV + "i");
+       if (strV.isBlank()){
+            counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes+
+                    "                                  No key value given");
+       }
+       else {
+        gtree.insert(parseInt(strV));
+        valueList.setText(valueList.getText()+" "+strV + "i");
+        counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes);
+
+       }
     }
     @FXML protected void searchButton(ActionEvent event) {
-       String strV=value.getText();
-       //Circle newC = new Circle(parseInt(strV));
-       gtree.tree.search(parseInt(strV));
-       valueList.setText(valueList.getText()+" "+strV + "s");
+        String strV=value.getText();
+        if (strV.isBlank()){
+            counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes+
+                    "                                  No key value given");
+        }
+        else {
+            String str = gtree.search(parseInt(strV));
+            valueList.setText(valueList.getText()+" "+strV + "s");
+            counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes+
+                    "               "+str);
+       }
     }
     @FXML protected void deleteButton(ActionEvent event) {
-       String strV=value.getText();
-       //Circle newC = new Circle(parseInt(strV));
-       gtree.tree.delete(parseInt(strV));
-       valueList.setText(valueList.getText()+" "+strV + "d");
+        String strV=value.getText();
+        if (strV.isBlank()){
+            counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes+
+                    "                                  No key value given");
+        }
+        else {
+        String str=gtree.delete(parseInt(strV));
+        valueList.setText(valueList.getText()+" "+strV + "d");
+        counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes+
+                "               "+str);
+        }
     }
     @FXML protected void clearButton(ActionEvent event) {
-       gtree.tree.clear();
+       gtree.clear();
        valueList.setText("");
+       counters.setText("Height: "+gtree.tree.height + " No. of Nodes: "+gtree.tree.noOfNodes);
     }
     @FXML protected void inorderButton(ActionEvent event) {
-        gtree.tree.inorder();
+        gtree.inorder(' ');
+        counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes+"          "+"Inorder Traversal:   "+
+                gtree.listTraversed);
     }
     @FXML protected void preorderButton(ActionEvent event) {
-        gtree.tree.preorder();
+        gtree.preorder();
+        counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes+"          "+"Preorder Traversal:   "+
+                gtree.listTraversed);
     }
     @FXML protected void postorderButton(ActionEvent event) {
-        gtree.tree.postorder();
+        gtree.postorder();
+        counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes+"          "+"Postorder Traversal:   "+
+                gtree.listTraversed);
     }
-    
-    
+    @FXML protected void sortASCButton(ActionEvent event) {
+        gtree.inorder(' ');
+        counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes+"          "+"Sorted List Ascending:   "+
+                gtree.listTraversed);
+    }
+    @FXML protected void sortDESCButton(ActionEvent event) {
+        gtree.inorder('r');
+        counters.setText("Height: "+gtree.tree.height + "  No. of Nodes: "+gtree.tree.noOfNodes+"          "+"Sorted List Descending:   "+
+                gtree.listTraversed);
+    }
 }
   
--- Tree.java
+++ Tree.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package assignment3;
+import java.awt.Paint;
 import java.util.ArrayList;
+import javafx.scene.paint.Color;
 //import javafx.scene.shape.Circle;
 /**
  *
@@ -15,10 +17,19 @@ class Node{
     public int key;  
     public Node left;
     public Node right;
+    public Color color;
+    
+    Node(int key,Color clr){
+        this.key=key;
+        this.left=null;
+        this.right=null;
+        color=clr;
+    }
     Node(int key){
         this.key=key;
         this.left=null;
         this.right=null;
+        color=Color.LIGHTGREEN;
     }
     Node(){
         this.left=null;
@@ -28,43 +39,45 @@ class Node{
 
 public class Tree {
     
-    public static Node root; 
+    public Node root; 
     public int height;
     public int noOfNodes;
     //functions to be implemented
 
     //default constructor
     Tree()
-    {   root=null;
-        
+    {   
+        root=null;
+        height=0;
+        noOfNodes=0;
     }
     //constructor with arraylist of nodes
     //insert an element in BST
-    Node insertTree(Node root, int key){
-        if(root==null){
-            Node leaf=new Node(key);
-            root=leaf;
-            return root;
+    Node insertTree(Node n, int key){
+        if(n==null){
+            Node leaf=new Node(key,Color.CORAL);
+            n=leaf;
+            return n;
         }
         else{
-            if (key < root.key) 
-            root.left = insertTree(root.left, key); 
-                else if (key > root.key) 
-            root.right = insertTree(root.right, key); 
+            if (key <= n.key) 
+            n.left = insertTree(n.left, key); 
+                else if (key > n.key) 
+            n.right = insertTree(n.right, key); 
 
         /* return the (unchanged) node pointer */
-        return root; 
+        return n; 
         }
     }
     int height(){
         return (treeHeight(root));
     }
-    int treeHeight(Node root){
-        if(root == null){
+    int treeHeight(Node n){
+        if(n == null){
             return 0;
         }
-        int left=treeHeight(root.left)
-;       int right=treeHeight(root.right);
+        int left=treeHeight(n.left);
+        int right=treeHeight(n.right);
         if(left>right){
             return (left+1);
         }
@@ -75,18 +88,21 @@ public class Tree {
     void insert(int element)
     {   
         root=insertTree(root,element);
+        noOfNodes++;
         height=height();
-        // code here
     }
-    Node searchTree(Node root,int key){
-        if (root==null || root.key==key) 
-        {   return root; 
-            }
-        if (root.key < key) 
-            {
-            return searchTree(root.right, key); 
-            }
-        return searchTree(root.left, key); 
+    Node searchTree(Node n,int key){
+        if (n==null)
+            return n;
+        if(n.key==key){
+            n.color=Color.CYAN;
+            return n;    
+        }
+            
+        if (n.key < key) 
+            return searchTree(n.right, key); 
+            
+        return searchTree(n.left, key); 
     }
     //searching an element
     boolean search(int element)
@@ -98,76 +114,83 @@ public class Tree {
     }
 
     //deleting an element
- void delete(int elem){
-        root=deleteTree(root,elem);
+    boolean delete(int elem){
+        boolean found[]={false};
+        root=deleteTree(root,elem,found);
         height=height();
+        return found[0];
     }
-    Node insertDel(Node root,Node leaf){
-        if(root==null){
-            root=leaf;
-            return root;
-        }
-        else{
-            if (leaf.key < root.key) 
-            root.left = insertDel(root.left, leaf); 
-                else if (leaf.key > root.key) 
-            root.right = insertDel(root.right, leaf); 
+    Node insertDel(Node n){
+        Node parent=n;
+        Node rt=n.right;
+        Node child= rt.left;
 
-        return root; 
+        while (child!=null)
+        {
+            parent=rt;
+            rt=rt.left;
+            child=rt.left;
         }
+        n.key=rt.key;
+        parent.left=rt.right;
+        return n; 
     }
-    Node deleteTree(Node root,int elem){
-        if(elem< root.key){
-            root.left=deleteTree(root.left,elem);
+    Node deleteTree(Node n,int elem, boolean [] found){
+        if (n==null){
+            found[0]=false;
+            n= null;
         }
-        else if(elem>root.key){
-            root.right=deleteTree(root.right,elem);
+        else if(elem< n.key){
+            n.left=deleteTree(n.left,elem, found);
         }
-        else if(elem==root.key &&(root.left==null && root.right==null)){
- 
-            if(root.left==null && root.right==null){
-                root=null;
+        else if(elem>n.key){
+            n.right=deleteTree(n.right,elem, found);
+        }
+        else if(elem==n.key) {
+            found[0]=true;
+            noOfNodes--;
+            if(n.left==null && n.right==null){
+                n= null;
+                }     
+            else if(n.right!=null && n.left!=null){
+                n=insertDel(n);
+
+                }
+            else if(n.right!=null && n.left==null){
+                n=n.right;
+                }
+            else if(n.right==null && n.left!=null){
+                n=n.left;
+                }
             }
-        }
-        else if(elem==root.key &&(root.right!=null && root.left!=null)){
-            root.right=insertDel(root.right,root.left);
-            root=root.right;
-
-        }
-        else if(elem==root.key &&(root.right!=null && root.left==null)){
-            root=root.right;
-        }
-        else if(elem==root.key &&(root.right==null && root.left!=null)){
-            root=root.left;
-        }
-        return root;
+        return n;
     }
-    ArrayList<Integer> inorder(ArrayList<Integer> p,Node root)
+    ArrayList<Integer> inorder(ArrayList<Integer> p,Node n)
     {
-        if (root != null) { 
-            inorder(p,root.left); 
-            p.add(root.key); 
-            inorder(p,root.right); 
+        if (n != null) { 
+            inorder(p,n.left); 
+            p.add(n.key); 
+            inorder(p,n.right); 
         } 
         return p;
     }
     //postorder traversal
-    ArrayList<Integer> postorder(ArrayList<Integer> p,Node root)
+    ArrayList<Integer> postorder(ArrayList<Integer> p,Node n)
     {
-        if (root != null) { 
-            postorder(p,root.left); 
-            postorder(p,root.right); 
-            p.add(root.key); 
+        if (n != null) { 
+            postorder(p,n.left); 
+            postorder(p,n.right); 
+            p.add(n.key); 
             
         } 
         return p;
     }
-    ArrayList<Integer> preorder(ArrayList<Integer> p,Node root)
+    ArrayList<Integer> preorder(ArrayList<Integer> p,Node n)
     {
-        if (root != null) { 
-            p.add(root.key); 
-            preorder(p,root.left); 
-            preorder(p,root.right); 
+        if (n != null) { 
+            p.add(n.key); 
+            preorder(p,n.left); 
+            preorder(p,n.right); 
         } 
         return p;
     }
@@ -175,7 +198,9 @@ public class Tree {
     //clear the tree 
     void clear()
     {
-         root=null;
+        root=null;
+        height = 0;
+        noOfNodes = 0;
     }
     //is empty
     boolean isEmpty()
